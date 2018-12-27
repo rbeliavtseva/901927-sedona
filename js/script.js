b6c2a3109559636e182ff12b609e396692b967a4
Rita <rbeliavceva@gmail.com>
2018-12-27 16:47:48 +0300

    Исправление ошибок

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 var button = document.querySelector(".toggle-modal-button");
 var modal = document.querySelector(".search-modal");
+var form = modal.querySelector("form");
+var arrivalDate = form.querySelector("[name=arrival-date]");
+var departureDate = form.querySelector("[name=departure-date]");
+var adults = form.querySelector("[name=adults]");
 
 button.addEventListener("click", function (evt) {
     evt.preventDefault();
     modal.classList.toggle("modal-show");
-});          
+    modal.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!arrivalDate.value || !departureDate.value || adults.value < 1) {
+        evt.preventDefault();
+        modal.classList.remove("modal-error");
+        modal.offsetWidth = modal.offsetWidth;
+        modal.classList.add("modal-error");
+    } 
+});
